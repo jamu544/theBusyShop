7c638b7df42e7469e6ec7687965075d9e175b7ce
odwa <odwa.menziwa@jumpco.io>
2022-06-06 16:15:12 +0200

    added a alert dialog to confirm added items to cart

--- app/src/main/java/android/com/jamsand/io/thebusyshop/views/ProductActivity.java
+++ app/src/main/java/android/com/jamsand/io/thebusyshop/views/ProductActivity.java
@@ -1,15 +1,19 @@
 package android.com.jamsand.io.thebusyshop.views;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.com.jamsand.io.thebusyshop.R;
 import android.com.jamsand.io.thebusyshop.utilities.Constants;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.com.jamsand.io.thebusyshop.views.MainActivity.context;
 
 public class ProductActivity extends AppCompatActivity {
 
@@ -49,6 +53,8 @@ public class ProductActivity extends AppCompatActivity {
 
     public void addToCartOnClick(View view){
 
+
+
         String barcodeName = barcodeTextView.getText().toString();
         String description = descriptionTextView.getText().toString();
        // double price = Double.parseDouble(priceTextView.getText().toString());
@@ -67,17 +73,43 @@ public class ProductActivity extends AppCompatActivity {
         data.putExtra(Constants.EXTRA_DESCRIPTION, description);
         data.putExtra(Constants.EXTRA_PRICE, price);
         data.putExtra(Constants.EXTRA_IS_CHECKED, true);
-//
+
         int id = getIntent().getIntExtra(Constants.EXTRA_ID,-1);
         if(id != -1){
             data.putExtra(Constants.EXTRA_ID,id);
         }
 
-        setResult(RESULT_OK, data);
-        finish();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add to Cart");
+        builder.setCancelable(false);
+        //builder.setMessage("My Message");
+        builder.setPositiveButton("OK", (dialog, which) -> {
 
-        Toast.makeText(ProductActivity.this, "added to Cart"+quantity, Toast.LENGTH_SHORT).show();
+            setResult(RESULT_OK, data);
+            finish();
 
+            Toast.makeText(ProductActivity.this, "added to Cart"+quantity, Toast.LENGTH_SHORT).show();
+             });
+        builder.setNegativeButton("Cancel", (dialog, which) -> { /* ... */ });
+
+        AlertDialog dialog = builder.create();
+
+        //Show the AlertDialog
+        dialog.show();
+
+
+
+
+    }
+
+    public void checkOutOnClick(View view ){
+        Intent intent = new Intent(ProductActivity.this, SummaryActivity.class);
+        startActivity(intent);
+        Toast.makeText(ProductActivity.this, "Check-Out", Toast.LENGTH_SHORT).show();
+    }
+    public double calculateProduct(double price,int quantity){
+        double sum = price * quantity;
+        return sum;
     }
 
 }
---
+++ app/src/main/java/android/com/jamsand/io/thebusyshop/views/SummaryActivity.java
@@ -0,0 +1,15 @@
+package android.com.jamsand.io.thebusyshop.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.com.jamsand.io.thebusyshop.R;
+import android.os.Bundle;
+
+public class SummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+}
